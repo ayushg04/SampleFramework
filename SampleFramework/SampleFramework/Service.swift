fd7861d2848d273fd013ff6a4e1902e082f7654c
Ayush Gaur <ayush.gaur@unthinkable.co>
2021-05-07 11:59:36 +0530

    Built our Sample Framework

---
+++ SampleFramework/SampleFramework/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  SampleFramework
+//
+//  Created by daffolapmac-179 on 07/05/21.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    public static func doSomething() -> String {
+        return "Hello There, How are you?"
+    }
+}
